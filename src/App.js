072ab8e057c3e186c7ee996caba2906db46b9dd7
Fabien SHAN <shan.fabien@gmail.com>
2018-05-31 12:47:13 +1200

    Initial playerBar structure & CSS reset file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './CSS-reset.css';
 import './Player.css';
 
 class App extends Component {
@@ -14,8 +15,13 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p> */}
-        <p>Hello</p>
-        <div id="player"></div>
+        <footer className="playerBar-container">
+          <div className="playerBar">
+            <div className="player__left">left</div>
+            <div className="player__center">center</div>
+            <div className="player__right">right</div>
+          </div>
+        </footer>
       </div>
     );
   }
