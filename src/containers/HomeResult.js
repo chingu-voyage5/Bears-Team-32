1f748650000cf189a2c166df4ae4ca20b6521632
Jack Yeh <jack.ffy@gmail.com>
2018-07-02 10:14:43 +1200

    Cancel axios request when unmounting component

--- src/containers/HomeResult.js
+++ src/containers/HomeResult.js
@@ -3,13 +3,32 @@ import axios from 'axios';
 import Card from '../components/Card';
 import Layout from '../components/Layout';
 
+const CancelToken = axios.CancelToken;
+const source = CancelToken.source();
+
 class HomeResult extends Component {
   state = { data: [] };
 
   componentDidMount() {
-    axios.get(this.props.api).then(({ data }) => {
-      this.setState({ data: data.items });
-    });
+    axios
+      .get(this.props.api, {
+        cancelToken: source.token,
+      })
+      .then(({ data }) => {
+        this.setState({ data: data.items });
+      })
+      .catch(error => {
+        if (axios.isCancel(error)) {
+          console.log('Request canceled', error.message);
+        } else {
+          console.log(error.message);
+        }
+      });
+  }
+
+  componentWillUnmount() {
+    // Cancel request when component is unmounting before request is completed
+    source.cancel('Operation canceled by the user.');
   }
 
   getItems = () => {
